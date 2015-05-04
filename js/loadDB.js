be341a696942a5470424b8c7235b68adc05b80db
unknown <Xiao.Tan@TOR-XTAN-LW7.na.audatex.com>
2015-05-04 17:02:14 -0400

    included db file

---
+++ js/loadDB.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '/path/to/database.sqlite', true);
+xhr.responseType = 'arraybuffer';
+
+xhr.onload = function(e) {
+  var uInt8Array = new Uint8Array(this.response);
+  var db = new SQL.Database(uInt8Array);
+  var contents = db.exec("SELECT * FROM my_table");
+  // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
+};
+xhr.send();
